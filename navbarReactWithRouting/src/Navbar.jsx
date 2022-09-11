b375086c5f7fd704707476b3fc5c4220ff405e9e
Alois Sailer <alois_sailer@yahoo.com.br>
2022-09-11 14:16:14 -0300

    construindo navbar

--- navbarReactWithRouting/src/Navbar.jsx
+++ navbarReactWithRouting/src/Navbar.jsx
@@ -1,10 +1,14 @@
 export default function Navbar() {
   return <nav className="nav">
-    <a href="/" className="site-title">Sailerdev</a>
+    <a href="/" className="site-title">Sailer<span className="span-title">dev</span></a>
     <ul>
       <li>
         <a href="/about">Sobre mim</a>
+      </li>
+      <li>
         <a href="/projects">Projetos</a>
+      </li>
+      <li>
         <a href="/skills">Habilidades</a>
       </li>
     </ul>
